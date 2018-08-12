ce7cdb81209e1bc58153142296ebbca806461c90
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-08-12 13:14:41 +0600

    added constant adding

---
+++ src/injector.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+
+function createInjector(modulesToLoad) {
+  var cache = {};
+  var $provide = {
+    constant: function(key, value) {
+      constants[key] = value;
+    }
+  };
+
+  _.forEach(modulesToLoad, function(moduleName) {
+    var module = window.angular.module(moduleName);
+    _.forEach(module._invokeQueue, function(invokeArgs) {
+      var method = invokeArgs[0];
+      var args = invokeArgs[1];
+      $provide[method].apply($provide, args);
+    });
+  });
+  return {
+    has: function(key) {
+      return cache.hasOwnProperty(key);
+    }
+  };
+}
+
+module.exports = createInjector;
--- src/loader.js
+++ src/loader.js
@@ -1,32 +1,39 @@
-function setupModuleLoader(window){
+var setupModuleLoader = function(window) {
   var ensure = function(obj, name, factory) {
     if (!obj[name]) {
       obj[name] = factory();
     }
     return obj[name];
   }
+
   var angular = ensure(window, 'angular', Object);
 
-  var module = ensure(angular, 'module', function(){
+  ensure(angular, 'module', function(){
     var modules = {};
-    return function(name, requires){
-      if (!requires) {
-        return getModule(name, modules);
-      } else {
+    return function(name, requires) {
+      if (requires) {
         return createModule(name, requires, modules);
+      } else {
+        return getModule(name, modules);
       }
     };
   });
 
   function createModule(name, requires, modules) {
     if (name === 'hasOwnProperty') {
-      throw 'name cannot be hasOwnProperty';
+      throw 'cannot be hasOwnProperty name';
     }
+
+    var invokeQueue = [];
 
     var moduleInstance = {
       name: name,
-      requires: requires
-    }
+      requires: requires,
+      constant: function(key, value) {
+        invokeQueue.push(['constant', [key, value]]);
+      },
+      _invokeQueue: invokeQueue
+    };
 
     modules[name] = moduleInstance;
 
@@ -34,13 +41,11 @@ function setupModuleLoader(window){
   }
 
   function getModule(name, modules) {
-    if (modules.hasOwnProperty(name)) {
-      return modules[name];
-    } else {
-      throw 'module dont exist';
+    if (!modules.hasOwnProperty(name)) {
+      throw 'No such module';
     }
+    return modules[name];
   }
-
-}
+};
 
 module.exports = setupModuleLoader;
