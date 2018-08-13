dbf3cc89550e2a63f9c549206144e75f7ad7936a
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-08-13 09:22:54 +0600

    added multiple

--- src/injector.js
+++ src/injector.js
@@ -2,23 +2,34 @@ var _ = require('lodash');
 
 function createInjector(modulesToLoad) {
   var cache = {};
+  var modules = {};
   var $provide = {
     constant: function(key, value) {
-      constants[key] = value;
+      if (key === 'hasOwnProperty') {
+        throw 'Cannot have such a name';
+      }
+      cache[key] = value;
     }
   };
 
-  _.forEach(modulesToLoad, function(moduleName) {
-    var module = window.angular.module(moduleName);
-    _.forEach(module._invokeQueue, function(invokeArgs) {
-      var method = invokeArgs[0];
-      var args = invokeArgs[1];
-      $provide[method].apply($provide, args);
-    });
+  _.forEach(modulesToLoad, function loadModules(moduleName) {
+    if (!modules.hasOwnProperty(moduleName)) {
+      modules[moduleName] = true;
+      var module = window.angular.module(moduleName);
+      _.forEach(module.requires, loadModules);
+      _.forEach(module._invokeQueue, function(invokeArgs) {
+        var method = invokeArgs[0];
+        var args = invokeArgs[1];
+        $provide[method].apply($provide, args);
+      });
+    }
   });
   return {
     has: function(key) {
       return cache.hasOwnProperty(key);
+    },
+    get: function(name) {
+      return cache[name];
     }
   };
 }
