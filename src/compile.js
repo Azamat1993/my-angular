515d1b3170599bb7cb35d8511bdd7974dcac4c91
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2018-09-20 10:08:28 +0600

    added basic compiling of node

--- src/compile.js
+++ src/compile.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var $ = require('jquery');
 function $CompilerProvider($provide) {
   var hasDirectives = {};
 
@@ -24,8 +25,45 @@ function $CompilerProvider($provide) {
     }
   }
 
-  this.$get = function(){
+  this.$get = function($injector){
+    function compile($compileNodes) {
+      return compileNodes($compileNodes);
+    }
 
+    function compileNodes($compileNodes) {
+      _.forEach($compileNodes, function(node) {
+        var directives = collectDirectives(node);
+        applyDirectivesToNode(directives, node);
+      });
+    }
+
+    function applyDirectivesToNode(directives, compileNode) {
+      var $compileNode = $(compileNode);
+      _.forEach(directives, function(directive){
+        if (directive.compile) {
+          directive.compile($compileNode);
+        }
+      })
+    }
+
+    function collectDirectives(node) {
+      var directives = [];
+      var normalizedNodeName = _.camelCase(nodeName(node).toLowerCase());
+      addDirective(directives, normalizedNodeName);
+      return directives;
+    }
+
+    function nodeName(node) {
+      return node.nodeName ? node.nodeName : node[0].nodeName;
+    }
+
+    function addDirective(directives, name) {
+      if (hasDirectives.hasOwnProperty(name)) {
+        directives.push.apply(directives, $injector.get(name + 'Directive'));
+      }
+    }
+
+    return compile;
   }
 }
 
--- test/compile.spec.js
+++ test/compile.spec.js
@@ -1,12 +1,20 @@
 var _ = require('lodash');
 var publishExternalAPI = require('../src/angular_public');
 var createInjector = require('../src/injector');
+var $ = require('jquery');
 
 describe('$compile', function(){
   beforeEach(function(){
     delete window.angular;
     publishExternalAPI();
   });
+
+  function makeInjectorWithDirectives() {
+    var args = arguments;
+    return createInjector(['ng', function($compileProvider) {
+      $compileProvider.directive.apply($compileProvider, args);
+    }])
+  }
 
   it('allows creating directives', function(){
     var myModule = window.angular.module('myModule', []);
@@ -47,5 +55,39 @@ describe('$compile', function(){
     expect(injector.has('aDirective')).toBe(true);
     expect(injector.has('bDirective')).toBe(true);
     expect(injector.has('cDirective')).toBe(true);
+  });
+
+  it('compiles element directives from a single element', function(){
+    var injector = makeInjectorWithDirectives('myDirective', function(){
+      return {
+        compile: function(element) {
+          element.data('hasCompiled', true);
+        }
+      }
+    })
+
+    injector.invoke(function($compile) {
+      var el = $('<my-directive></my-directive>');
+      $compile(el);
+      expect(el.data('hasCompiled')).toBe(true);
+    });
+  });
+
+  it('compiles element directives found from several elements', function(){
+    var idx = 1;
+    var injector = makeInjectorWithDirectives('myDirective', function(){
+      return {
+        compile: function(element) {
+          element.data('hasCompiled', idx++);
+        }
+      }
+    });
+
+    injector.invoke(function($compile) {
+      var el = $('<my-directive></my-directive><my-directive></my-directive>');
+      $compile(el);
+      expect(el.eq(0).data('hasCompiled')).toBe(1);
+      expect(el.eq(1).data('hasCompiled')).toBe(2);
+    })
   });
 });
