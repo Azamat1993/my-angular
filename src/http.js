a615da7695e0ca4bf5c22447b9db4ab8df21e46c
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2019-01-18 17:55:08 +0600

    added some more tests

--- src/http.js
+++ src/http.js
@@ -1,9 +1,32 @@
 'use strict';
+var _ = require('lodash')
 
 function $HttpProvider() {
+  var defaults = this.defaults = {
+    headers: {
+      common: {
+        Accept: 'application/json, text/plain, */*'
+      },
+      post: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      put: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      patch: {
+        'Content-Type': 'application/json;charset=utf-8'
+      }
+    }
+  }
+
   this.$get = ['$httpBackend', '$q', '$rootScope', function($httpBackend, $q, $rootScope) {
-    return function $http(config) {
+    function $http(requestConfig) {
       var deferred = $q.defer();
+
+      var config = _.extend({
+        method: 'GET'
+      }, requestConfig);
+      config.headers = mergeHeaders(requestConfig);
 
       function isSuccess(status) {
           return status >= 200 && status <= 299;
@@ -23,9 +46,21 @@ function $HttpProvider() {
         }
       }
 
-      $httpBackend(config.method, config.url, config.data, done);
+      function mergeHeaders(config) {
+        return _.extend(
+          {},
+          defaults.headers.common,
+          defaults.headers[(config.method || 'get').toLowerCase()],
+          config.headers
+        )
+      }
+
+      $httpBackend(config.method, config.url, config.data, done, config.headers);
       return deferred.promise;
     }
+
+    $http.defaults = defaults;
+    return $http;
   }]
 }
 
