6c6de5004ea45353e25f2c1e157a0de9fcad52ec
Azamat Zorkanov <Azamat_Zorkanov@epam.com>
2019-01-18 10:17:11 +0600

    added resolving promise when request in done

--- src/http.js
+++ src/http.js
@@ -1,10 +1,29 @@
 'use strict';
 
 function $HttpProvider() {
-  this.$get = ['$httpBackend', '$q', function($httpBackend, $q) {
+  this.$get = ['$httpBackend', '$q', '$rootScope', function($httpBackend, $q, $rootScope) {
     return function $http(config) {
       var deferred = $q.defer();
-      $httpBackend(config.method, config.url, config.data);
+
+      function isSuccess(status) {
+          return status >= 200 && status <= 299;
+      }
+
+      function done(status, response, statusText) {
+        status = Math.max(status, 0);
+        deferred[isSuccess(status) ? 'resolve' : 'reject']({
+          status: status,
+          data: response,
+          statusText: statusText,
+          config: config
+        });
+
+        if (!$rootScope.$$phase) {
+          $rootScope.$apply();
+        }
+      }
+
+      $httpBackend(config.method, config.url, config.data, done);
       return deferred.promise;
     }
   }]
